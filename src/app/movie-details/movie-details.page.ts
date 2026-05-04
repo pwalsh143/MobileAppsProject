bb0a7369572d242d831650e4755d5086af828f29
Paul Walsh <p.walsh9999@gmail.com>
2026-05-04 23:35:21 +0100

    Added api key with concated url and pulled info like title and images succesfully. Some trial and error but working now. Tested first with movie details page will have to switch over code so it works on homepage next.

--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/angular/standalone';
+import { DataService } from '../services/data.service';
+import { MyHttpService } from '../services/my-http.service';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   styleUrls: ['./movie-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent]
 })
 export class MovieDetailsPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  keyword: string = "";
+  apiKey = "1e83ad3775d3523cef62b909a9826f44"
+  movieInfo: any;
+  options: HttpOptions = {
+  url: "https://api.themoviedb.org/3/trending/movie/day?api_key=" + this.apiKey 
   }
+  
+  constructor(private ds:DataService, private mhs:MyHttpService) { }
 
+   ngOnInit() {
+    this.getKW();
+   }
+
+    async getKW(){
+    this.keyword = await this.ds.get('kw');
+    this.options.url = this.options.url.concat(this.keyword)
+    let result = this.mhs.get(this.options)
+    this.movieInfo = (await result).data.results
+    console.log(JSON.stringify(this.movieInfo)) 
+  }
 }
+
